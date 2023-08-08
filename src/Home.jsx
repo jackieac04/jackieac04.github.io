a285742a54f3506ff4d066bc21ddfe6f042a7d63
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-08 14:18:02 -0400

    gmail icon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Web from '/Web.png'
 import Butterflies from '/ButterfliesE.gif'
+import Gmail from '/icons/Gmail.png'
 
 export default function Home() {
   return (
     <div className='home-container'>
-        <img src={ Web } alt="" />
-        <img className="butterflies" src={ Butterflies } alt="" />
+        <img className="web" src={ Web } alt="A digital painting of a girl overlooking a well, surrounded by lush plant life and several animals. The text says: 'About me: 
+        Hi there! My name is Jaclyn Cohen, but you can call me Jackie :)
+        I'm a visual artist from South Florida, but I am currently based in Providence, Rhode Island where I study computer science at Brown University.
+        I'm super interested in the intersections between art and computation, which is why I made this site!
+        If you fall further down the well, you'll find my contact info. 
+        Definitely send me a message if you can find all of the animals on this page.
+        That's all for now; happy exploring!' " />
+        <div className='web-content'>
+            <img className="butterflies" src={ Butterflies } alt="a gif of butterflies overlaying the background image." />
+            <a className='gmail-link' href="mailto: jaclyn_cohen@brown.edu">
+                <img  className="gmail" src={ Gmail} alt="the gmail app icon" />
+            </a>
+        </div>
+        
 
     </div>
   )
