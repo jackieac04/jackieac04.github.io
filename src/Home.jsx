aa8e2ae5c31a031d75982c2d6e97a2a975ddc074
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-07 21:38:14 -0400

    got rid of unnecessary .

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     // displays the main BG img and overlaying gif
     <div className='home-container'>
-        <img className="home" src=".docs/assets/images/HomePage.png" alt="A digital painting of a girl looking down a well. She is surrounded by plants and animals. Halfway down the well, text reads: About Me. 
+        <img className="home" src="docs/assets/images/HomePage.png" alt="A digital painting of a girl looking down a well. She is surrounded by plants and animals. Halfway down the well, text reads: About Me. 
         Hi there! My name is Jaclyn Cohen but you can call me Jackie :)
         I'm a visual artist from South Florida, but I am currently based in Providence, Rhode Island where I study computer science at Brown University.
         I'm super interested in the intersections between art and computation, which is why I made this site!
