2dfaedc5c712200c07146ca77671a1b350147f91
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-08 10:28:20 -0400

    again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    jsxInject: `import React from 'react'`
+    jsxInject: `import React from 'react'`,
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
   }
 });
