0cc43feb5a70ea5dff1422a2d71ad2d7e91fba74
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-08 13:03:15 -0400

    add gif on top of img

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Web from '/Web.png'
+import Butterflies from '/ButterfliesE.gif'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home-container'>
         <img src={ Web } alt="" />
+        <img className="butterflies" src={ Butterflies } alt="" />
+
     </div>
   )
 }
