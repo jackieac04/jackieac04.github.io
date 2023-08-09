c7526f0ed9e4ac880b473082075467671e5fc721
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-09 08:59:10 -0400

    header and footer imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Home from './Home'
+import Header from '/Header_.png'
+import Footer from '/Footer.png'
 
 function App() {
 
   return (
     <>
+    <div className='app-container'>
+    <nav className='header-nav'>
+      <img className="header-img" src={ Header } alt="" />
+    </nav>
       <Home />
+      <nav className='footer-nav'>
+        <img className='footer-img' src={ Footer } alt="" />
+      </nav>
+    </div>
     </>
   )
 }
