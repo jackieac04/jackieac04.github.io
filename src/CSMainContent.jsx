f4ed4fe2470c544b73548c616f5e7244c870a916
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-12 18:02:44 -0400

    cs main page

--- src/CSMainContent.jsx
+++ src/CSMainContent.jsx
@@ -7,9 +7,38 @@ export default function CSMainContent() {
     <div className='cs-main'>
         <div className='info'>
         <h1>About my Work</h1>
-        <p> Unlike art, I didn't begin coding until fairly recently, Spring 2022, to be more specific. </p>
-   
-        </div>
+        <p className='cs-text'> Unlike art, I didn't begin coding until fairly recently, Spring 2022, to be more specific.
+        In high school I was focused on my art practice, and we didn't have much in the way of stem resources
+        or any computer science related courses until my senior year.
+        <br />
+        <br />
+        Because I had such little exposure to it, I really didn't know anything about the subject, at all. But as I was
+        <a href="https://bulletin.brown.edu/the-college/concentrations/">the many undergrad concentrations one could choose at Brown</a>, I discovered why Computer Science is undoubtedly 
+        the most popular concentration for undergrads here.
+        <br />
+        <br />
+        I've always loved math and science and its applications, and I realized quickly by looking through the <a href="https://bulletin.brown.edu/the-college/concentrations/comp/">Course Catalog</a> that 
+        I had been mistaken before. So many classes looked interesting (most of which I have yet to take)- from Graphics to Computer Vision,
+        topics that bridged the gap between my many interdisciplinary interests. 
+        <br />
+        <br />
+        And so, hesitantly, I began my coding journey.
+        <br />
+        <br />
+        Entering freshman year, I was scared of what was ahead. I had learned some fundamentals over the summer to prepare, 
+        but the little had truly understood was no match for the pacing of the curriculum, from the perspective of someone who knew almost nothing like I did.
+        But needless to say, I loved it. I loved making silly little games in <a href="https://cs.brown.edu/courses/cs015/">CS 15</a> and I even loved how satisfying it felt to finish completely non-visual projects in <a href="https://brown-csci0200.github.io/">CS 200</a>. 
+        And as the months went on, I began to spend less time in office hours and more time making things and really understanding how to use the concepts I had learned.
+        <br />
+        <br />
+        After the end of freshman year, I began as a research assistant at <a href="https://sites.brown.edu/bltlab/">The Brown Language and Thought Lab</a>. And although I had never worked in JavaScript before or on web apps,
+        working on projects that would actually be used to help people was so rewarding. 
+        <br />
+        <br />
+        Heading into sophomore year and the rest of college, I'm so excited to see what more I'll learn, in my courses and also as a Teaching Assistant for CS 15.
+        Thanks for reading! Check out some recent projects I've been working on above :)
+        </p>
+         </div>
       </div>
     </>
   )
