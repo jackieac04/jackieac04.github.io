a741b7c9f16610677204b412f2b5b53799811708
jackieac04 <jaclyn_cohen@brown.edu>
2023-08-13 14:01:38 -0400

    added links to cs main

--- src/CSMainContent.jsx
+++ src/CSMainContent.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Art.css'
+import tetris from '/tetris.gif'
+import susman from '/susman.gif'
+import MP7 from '/MP7.gif'
+import MP9 from '/MP9.gif'
+import Web from '/Web.png'
 
 export default function CSMainContent() {
   return (
@@ -7,37 +12,41 @@ export default function CSMainContent() {
     <div className='cs-main'>
         <div className='info'>
         <h1>About my Work</h1>
-        <p className='cs-text'> Unlike art, I didn't begin coding until fairly recently, Spring 2022, to be more specific.
-        In high school I was focused on my art practice, and we didn't have much in the way of stem resources
-        or any computer science related courses until my senior year.
+        <p className='cs-text'>
+        Although my cs journey started quite recently compared to my artistic one, I have many interests that I'm exploring.
+        I'm drawn to the visual side of computer science- web development, UI/UX design, graphics – anything that combines the logic of coding with visual creativity resonates with me. 
+        I'm also interested in how we can make technology both functional and aesthetically pleasing and user-friendly. 
         <br />
         <br />
-        Because I had such little exposure to it, I really didn't know anything about the subject, at all. But as I was
-        <a href="https://bulletin.brown.edu/the-college/concentrations/">the many undergrad concentrations one could choose at Brown</a>, I discovered why Computer Science is undoubtedly 
-        the most popular concentration for undergrads here.
+        After the end of freshman year, I began as a research assistant at <a href="https://sites.brown.edu/bltlab/">The Brown Language and Thought Lab</a> under <a href="https://www.gaborbrody.com/">Gabor Brody</a>. 
+        I love working on projects that are used to help people and further our knowledge of the mind!
         <br />
         <br />
-        I've always loved math and science and its applications, and I realized quickly by looking through the <a href="https://bulletin.brown.edu/the-college/concentrations/comp/">Course Catalog</a> that 
-        I had been mistaken before. So many classes looked interesting (most of which I have yet to take)- from Graphics to Computer Vision,
-        topics that bridged the gap between my many interdisciplinary interests. 
+        When I'm not coding or painting you'll probably find me exploring my surroundings or collecting little <a href="https://www.instagram.com/birbsatbrown/">birbs</a> or other trinkets, which is why I'm currntly woking on
+        <a href='/cs/poster'>The Brown Poster Archive</a>, in an attempt to catalog and document the history of student events, priorities, and design choices over time at Brown. 
         <br />
         <br />
-        And so, hesitantly, I began my coding journey.
-        <br />
-        <br />
-        Entering freshman year, I was scared of what was ahead. I had learned some fundamentals over the summer to prepare, 
-        but the little had truly understood was no match for the pacing of the curriculum, from the perspective of someone who knew almost nothing like I did.
-        But needless to say, I loved it. I loved making silly little games in <a href="https://cs.brown.edu/courses/cs015/">CS 15</a> and I even loved how satisfying it felt to finish completely non-visual projects in <a href="https://brown-csci0200.github.io/">CS 200</a>. 
-        And as the months went on, I began to spend less time in office hours and more time making things and really understanding how to use the concepts I had learned.
-        <br />
-        <br />
-        After the end of freshman year, I began as a research assistant at <a href="https://sites.brown.edu/bltlab/">The Brown Language and Thought Lab</a>. And although I had never worked in JavaScript before or on web apps,
-        working on projects that would actually be used to help people was so rewarding. 
-        <br />
-        <br />
-        Heading into sophomore year and the rest of college, I'm so excited to see what more I'll learn, in my courses and also as a Teaching Assistant for CS 15.
-        Thanks for reading! Check out some recent projects I've been working on above :)
+        Heading into sophomore year and the rest of college, I'm so excited to see what more I'll learn, in my courses and also as a Teaching Assistant for <a href="https://cs.brown.edu/courses/cs015/">CS 15</a> this fall.
+        Thanks for reading! Check out some recent projects I've been working on below :)
         </p>
+         </div>
+         <div className='sub-links'>
+            <a className='link' href="/cs/this">
+                <img className='gif one' src={Web} />
+            </a>
+            
+         <a className='link' href="/cs/tetrissus">
+            <img className='gif one' src={tetris} />
+            <img className='gif one' src={susman} />
+        </a>
+        <a className='link' href="/cs/mp">
+            <img className='gif MP7' src={MP7} />
+            <img className='gif MP7' src={MP9} />
+        </a>
+        <a className='link' href="/cs/dttp">
+            <img className='gif dt' src="/dt.gif" />
+            <img className='gif tp' src="/tp.png" />
+        </a>
          </div>
       </div>
     </>
--- src/CSOptions.jsx
+++ src/CSOptions.jsx
@@ -7,8 +7,8 @@ const options = [
         { name: 'Poster Archive - Coming soon', to: 'cs/poster', component: <CSSubContent /> },
         { name: 'This site!', to: 'cs/this', component: <CSSubContent /> },
         { name: 'Modal Perception', to: 'cs/mp', component: <CSSubContent /> },
-        { name: 'Tetris', to: 'cs/tetris', component: <CSSubContent />},
-        { name: 'AmongMan', to: 'cs/amongman', component: <CSSubContent /> }
+        { name: 'Tetris and PacMan', to: 'cs/tetrissus', component: <CSSubContent />},
+        { name: 'Decision Tree, Travel Planner', to: 'cs/dttp', component: <CSSubContent /> }
   ];
 
   export default options;
